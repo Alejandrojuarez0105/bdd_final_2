f6f5c4be75b0ce873a8501186db23bcd8930ec0c
Alejandrojuarez0105 <alejandrojuarez01050812@gmail.com>
2025-01-13 21:42:53 +0100

    Triggers, consultas grupales

--- consultas.sql
+++ consultas.sql
@@ -20,15 +20,16 @@ SELECT * FROM habitacion WHERE codigo LIKE 'A%' EXCEPT SELECT * FROM habitacion 
 SELECT tipo, AVG(tarifa_por_noche) AS tarifa_promedio, COUNT(*) AS cantidad_habitaciones, MAX(tarifa_por_noche) AS tarifa_maxima FROM habitacion GROUP BY tipo;
 
 -- Reunion natural --
-
+SELECT * FROM reserva_habitacion NATURAL JOIN habitacion;
 
 -- Reunion natural por la izquierda --
-
+SELECT * FROM reserva_habitacion LEFT JOIN habitacion ON reserva_habitacion.habitacion_id = habitacion.habitacion_id;
 
 -- Reunion natural por la derecha --
-
+SELECT * FROM habitacion RIGHT JOIN reserva_habitacion ON habitacion.habitacion_id = reserva_habitacion.habitacion_id;
 
 -- Producto cartesiano --
+SELECT * FROM reserva_habitacion CROSS JOIN habitacion;
 
 
 END //
@@ -176,3 +177,57 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+
+-- Triggers Alejandro Juarez --
+
+-- Trigger Individual Alejandro Juarez --
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS valida_habitaciones_numero_personas;
+CREATE TRIGGER IF NOT EXISTS valida_habitaciones_numero_personas
+BEFORE INSERT ON habitacion
+FOR EACH ROW
+BEGIN
+    -- Validar que tarifa_por_noche no sea NULL si el estado es 'ocupada' o 'en mantenimiento'
+    IF (NEW.estado IN ('ocupada', 'en mantenimiento') AND NEW.tarifa_por_noche IS NULL) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: La tarifa_por_noche no puede ser NULL cuando el estado es "ocupada" o "en mantenimiento".';
+    END IF;
+
+    -- Validar que la capacidad_maxima sea mayor a 0
+    IF (NEW.capacidad_maxima <= 0) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: La capacidad_maxima debe ser mayor que 0.';
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- Trigger Grupal Alejandro Juarez --
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS after_insert_reserva_habitacion;
+CREATE TRIGGER IF NOT EXISTS after_insert_reserva_habitacion
+AFTER INSERT ON reserva_habitacion
+FOR EACH ROW
+BEGIN
+    UPDATE habitacion
+    SET estado = 'ocupada'
+    WHERE habitacion_id = NEW.habitacion_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS after_delete_reserva_habitacion;
+CREATE TRIGGER IF NOT EXISTS after_delete_reserva_habitacion
+AFTER DELETE ON reserva_habitacion
+FOR EACH ROW
+BEGIN
+    UPDATE habitacion
+    SET estado = 'disponible'
+    WHERE habitacion_id = OLD.habitacion_id;
+END$$
+DELIMITER ;
+
